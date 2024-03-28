a1a194007151298e0f0f1cd5a7018ac5be520d99
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2024-03-27 23:33:45 -0300

    :imp: Implementação do Enumerado e injeção do construtor dos drivers
    Nesta atualização o implemento enumerado que representa as tecnologias de drivers de conexão e também crio um sistema de injeção do construtor dos drivers de conexão.

--- common/database.driver.abstract.pas
+++ common/database.driver.abstract.pas
@@ -4,18 +4,37 @@ interface
 
 uses
   System.SysUtils,
-  System.Classes;
+  System.Classes,
+  dao.abstract;
 
 type
-  TDatabaseDriverAbstract = class(TDataModule)
+  TDatabaseDriverAbstract = class sealed(TDataModule)
   private
+    FParamStr: string;
+    procedure SetParamStr(const Value: string);
     { Private declarations }
   public
+    constructor Create(AOwner: TComponent); override;
     { Public declarations }
+    property ParamStr: string read FParamStr write SetParamStr;
+
   end;
 
 implementation
 {%CLASSGROUP 'Vcl.Controls.TControl'}
 {$R *.dfm}
+
+{ TDatabaseDriverAbstract }
+
+constructor TDatabaseDriverAbstract.Create(AOwner: TComponent);
+begin
+  inherited;
+
+end;
+
+procedure TDatabaseDriverAbstract.SetParamStr(const Value: string);
+begin
+  FParamStr := Value;
+end;
 
 end.
--- common/model.abstract.pas
+++ common/model.abstract.pas
@@ -5,30 +5,44 @@ interface
 uses
   System.SysUtils,
   System.Classes,
-  Datasnap.Provider;
+  System.IoUtils,
+  IniFiles,
+  Datasnap.Provider,
+  database.driver.abstract;
 
-type
+ type TODriverTecnology = ( oDefaultDriver , oFirebirdDriver , oPostgresDriver ,  oOracleDriver );
+
+ type
   TModelAbstract = class(TDataModule)
     oProvider: TDataSetProvider;
-  private
+  protected
+    FConfigureDriverFirebird : TProc<TODriverTecnology,TInifile>;
+    FConfigureDriverPostgres : TProc<TODriverTecnology,TInifile>;
+    FConfigureDriverOracle   : TProc<TODriverTecnology,TInifile>;
+    FTDatabaseDriverAbstract : TDatabaseDriverAbstract;
+  strict private
     FTablename: String;
     FAutoincField: String;
     FFields: TArray<String>;
     FGeneratorName: string;
     FWhereClausule: TArray<String>;
+    FConnectionDriver: TODriverTecnology;
     procedure SetAutoincField(const Value: String);
     procedure SetFields(const Value: TArray<String>);
     procedure SetGeneratorName(const Value: string);
     procedure SetTablename(const Value: String);
     procedure SetWhereClausule(const Value: TArray<String>);
+    procedure SetConnectionDriver(const Value: TODriverTecnology);
     { Private declarations }
   public
+    constructor Create(AOwner: TComponent); override;
     { Public declarations }
     property Tablename: String read FTablename write SetTablename;
     property GeneratorName: string read FGeneratorName write SetGeneratorName;
     property AutoincField:String read FAutoincField write SetAutoincField;
     property Fields: TArray<String> read FFields write SetFields;
     property WhereClausule: TArray<String> read FWhereClausule write SetWhereClausule;
+    property ConnectionDriver: TODriverTecnology read FConnectionDriver write SetConnectionDriver;
   end;
 
 implementation
@@ -37,9 +51,47 @@ implementation
 
 { TModelAbstract }
 
+constructor TModelAbstract.Create(AOwner: TComponent);
+begin
+  inherited;
+  FTDatabaseDriverAbstract:= TDatabaseDriverAbstract.Create( AOwner );
+  ConnectionDriver:= oDefaultDriver;
+end;
+
 procedure TModelAbstract.SetAutoincField(const Value: String);
 begin
   FAutoincField := Value;
+end;
+
+procedure TModelAbstract.SetConnectionDriver(const Value: TODriverTecnology);
+var
+  LInifile: TInifile;
+begin
+  FConnectionDriver := Value;
+
+  FConfigureDriverFirebird:= procedure(PDriver: TODriverTecnology; Pnifile: TInifile)
+                             begin
+                              if PDriver <> oFirebirdDriver  then Exit;
+
+
+                             end;
+
+  FConfigureDriverPostgres:= procedure(PDriver: TODriverTecnology; Pnifile: TInifile)
+                             begin
+                              if PDriver <> oPostgresDriver  then Exit;
+
+                             end;
+
+  FConfigureDriverOracle  := procedure(PDriver: TODriverTecnology; Pnifile: TInifile)
+                             begin
+                              if PDriver <> oOracleDriver  then Exit;
+
+                             end;
+
+  FConfigureDriverFirebird(FConnectionDriver,LInifile);
+  FConfigureDriverPostgres(FConnectionDriver,LInifile);
+  FConfigureDriverOracle(FConnectionDriver,LInifile);
+
 end;
 
 procedure TModelAbstract.SetFields(const Value: TArray<String>);
